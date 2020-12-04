9c7e1917bfadad1393810dfdb754a7533478d26f
Jitong Ding <djt18919805555@gmail.com>
2020-12-03 20:32:16 -0500

    Update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,69 +1,62 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const { exec } = require('child_process')
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+var proc;
 
-const createWindow = () => {
-  let win = new BrowserWindow({ show: false })
-  win.once('ready-to-show', () => {
-  win.show()
-})
+function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width:1300,
-    height: 1200,
-    //kiosk: true,
-    // webSecurity: false,
+    width: 500,
+    height: 600,
     webPreferences: {
-      // Allows us to call nodejs globals in the frontend code
-      nodeIntegration: true,
-      enableRemoteModule: true,
-    },
-  });
-
-  /*let child = new BrowserWindow({ 
-    width:800,
-    height: 700,
-    kiosk: true,
-    webSecurity: false,
-    transparent: true,
-    frame: false,
-    parent: mainWindow })
-  child.loadFile(path.join(__dirname, 'login.html'));*/
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.on('close', () => { win = null })
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  mainWindow.webContents.openDevTools()
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  // Spawn python config subprocess
+  const capstone_path = "/home/felix/Capstone/";
+  const exec_str = `python ${capstone_path}py-audio/host.py`;
+  proc = exec(exec_str, (error, stdout, stderr) => {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: '+error.code);
+      console.log('Signal received: '+error.signal);
+    }
+    console.log('Child Process STDOUT: '+stdout);
+    console.log('Child Process STDERR: '+stderr);
+  });
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+app.on('window-all-closed', function () {
+  // Kill the python host process
+  proc.kill('SIGTERM');
+  if (process.platform !== 'darwin') app.quit();
+})
 
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-
-
+// code. You can also put them in separate files and require them here.
--- my-app/src/render.js
+++ my-app/src/render.js
@@ -1,101 +1,170 @@
-//const {BrowserWindow} = require('electron').remote
-//const newWindowBtn = document.getElementById('gesture-window')
-//const soundWinBtn = document.getElementById('sound-window')
-const fs = require('fs');
-const yaml = require('js-yaml');
-//const pythoNFile = document.getElementById('script')
-const { exec } = require('child_process');
-const submitButton = document.getElementById("submitButton");
+const fs = require("fs"); 
+const { type } = require('os');
+var tableID = 0;
+var gestures = ["ONE","TWO", "THREE", "FOUR","FIST","OK","YEAH","ROCK","SPIDERMAN","Slide_left","Slide_right","Scrolling_up","Scrolling_down","Voice"]
+var action_type = ["Email","OPEN_TERMINAL", "Text to speech","Switch desktop","Type","HOTKEY"]
+var voice_actors = ["Arnold Schwarzenegger","Bob Barker","Tucker Carlson"]
+window.onload = function(){
+    $('.message .close')
+    .on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
+    $('.ui.dropdown').dropdown();
+    let add_button = document.getElementById("add");
+    let save_button = document.getElementById("save");
+    add_button.addEventListener('click',event=>{
+      tableID += 1;
+      create_table_entry();
+      document.getElementById("actionChoice"+tableID.toString()).onchange = (event)=>{
+         let action_choice = $("#actionChoice" + tableID.toString()).val()
+          for(let i = 0;i<action_type.length;i++){
+      //      console.log(document.getElementById(action_type[i] + tableID.toString()))
+            if(document.getElementById(action_type[i] + "Form" + tableID.toString()) !=null){
+              document.getElementById("container"+tableID.toString()).removeChild(document.getElementById(action_type[i] + "Form" + tableID.toString()))
+            }
+            if(document.getElementById("dropDown" + action_type[2] + tableID.toString()) !=null){
+              document.getElementById("container"+tableID.toString()).removeChild(document.getElementById("dropDown"+action_type[2] + tableID.toString()))
 
-const path = require('path');
-let fileContents = fs.readFileSync('configuration.yml');
-let data = yaml.load(fileContents);
-console.log(data.VOICE);
-$('#fourteen option:contains(' + data.VOICE + ')').attr('selected', 'selected');
-//document.querySelector('#fourteen').textContent;
-//console.log(document.querySelector('#fourteen').value);
-// submitButton.addEventListener('click',(event)=>{
+            }
+          }
+          if(action_choice == action_type[2]){
+            let  formNode = addElement(document.getElementById("container"+tableID.toString()),"div","ui form",action_choice + "Form" + tableID.toString(),null,null,null,null)
+            let childNode = addElement(formNode,"div","field",null,null,null,null,null);
+            addElement(childNode,"label",null,null,action_choice,null,null,null);
+            addElement(childNode,"input",null,action_choice + tableID.toString(),null,null,null,"text")
+            let dropdownNode = addElement(document.getElementById("container"+tableID.toString()),"select","ui dropdown","dropDown" + action_choice + tableID.toString(),null,null,null,null)
+            for(let i=0; i<voice_actors.length;i++){
+              addElement(dropdownNode,"option",null,i,voice_actors[i],voice_actors[i],null,null)
+            }
+          }
+          else if(action_type.includes(action_choice,3)  ){
 
-// });
-
-var actions = ["Desktop one", "Desktop two", "Desktop three","Desktop four"];
-var id = ["first","second","third","fourth","five","six","seven","eight","nine","ten","eleven","twelve","thirteen"];
-for(var j =0; j < id.length;++j){
-  for(var i = 0; i < actions.length;++i){
-    // var x = document.getElementById("1");
-    var x = document.getElementById(id[j]);
-    var option = document.createElement("option");
-    option.text = actions[i];
-    console.log(j);
-    x.add(option, x[0]);
-  }
-  
+          let  formNode = addElement(document.getElementById("container"+tableID.toString()),"div","ui form",action_choice + "Form" + tableID.toString(),null,null,null,null)
+          let childNode = addElement(formNode,"div","field",null,null,null,null,null);
+          console.log(tableID)
+          addElement(childNode,"label",null,null,action_choice,null,null,null);
+          addElement(childNode,"input",null,action_choice + tableID.toString(),null,null,null,"text")
+        }
+        
+      };
+    });
+    save_button.addEventListener('click',(event)=>{
+      build_json()
+    });
 }
-/*newWindowBtn.addEventListener('click', (event) => {
-  const modalPath = path.join('file://', __dirname, '../../my-app/src/gesture.html')
-  let win = new BrowserWindow({ 
-    width: 1300, 
-    height:1200,
-    // kiosk: true,
-    // webSecurity: false,
-    // transparent: true,
-    // frame: false,
-    webPreferences: {
-      // Allows us to call nodejs globals in the frontend code
-      nodeIntegration: true,
-      enableRemoteModule: true,
-    },
-    
-     })
 
-  win.on('close', () => { win = null })
-  win.loadURL(modalPath)
-  win.show()
+
+function create_table_entry(){
+   let current_node = document.body;
+   current_node = addElement(current_node,"div","ui container","container" + tableID.toString(),null,null,null,null);
+   let childNode = addElement(current_node,"div","ui floating labeled icon dropdown button","action" + tableID.toString(), null,null,null,null); 
+    addElement(childNode,"i","add user icon",null,null,null,null,null);
+    addElement(childNode,"span","text",null,"Add action",null,null,null);
+    childNode = addElement(childNode,"div","menu","actionMenu" + tableID.toString(),null,null,null,null);
+    addElement(childNode,"div","header",null,"Gesture or Voice Command selection",null,null,null);
+    let i = 0
+    for(i; i < gestures.length;i++){
+        addElement(addElement(childNode,"div","item",i,gestures[i],gestures[i],null,null),"img","ui image",gestures[i],null,null,"One.jpg",null)
+    }
+    let formNode = addElement(current_node,"div","ui  form",null,null,null,null,null);
+    childNode = addElement(formNode,"div","field",null,null,null,null,null);
+    addElement(childNode,"label",null,null,"Voice Command Keyword(s)",null,null,null);
+    addElement(childNode,"input",null,"command" + tableID.toString(), null,null,null,"text")
+    childNode = addElement(current_node,"select","ui dropdown",id="actionChoice" + tableID.toString(),null,null,null,null)
+    for(let i=0; i<action_type.length;i++){
+        addElement(childNode,"option",null,i,action_type[i],action_type[i], null,null);
+    }
+    $('.ui.dropdown').dropdown();
+    //Change: Creating Remove button  
+    let butNode = addElement(current_node,"BUTTON","ui icon red button",tableID.toString(),null,null,null,null);
+    childNode = addElement(butNode,"i","minus circle icon",null,null,null,null,null);
+    
+}
+function addElement(current_node,tag_type,className=null,id=null,innerHTML=null,
+                    nodeValue=null,src=null,nodeType=null){
+    let newNode = document.createElement(tag_type);
+    if(id != null){
+        newNode.id = id;
+    }
+    if(className != null){
+        newNode.className = className;
+    }
+    if(innerHTML != null){
+        newNode.innerHTML = innerHTML;
+    }
+    if(nodeValue != null){
+        newNode.nodeValue = nodeValue;
+    }
+    if(src !=null){
+        newNode.src = src;
+    }
+    if(nodeType != null){
+        newNode.type = nodeType;
+    }
+
+    //Change: Remove button function
+    if(tag_type=="BUTTON"){
+      newNode.addEventListener('click',(event)=>{
+        var node = document.getElementById("container"+id);
+        node.remove()
+      });
+    }
+    current_node.appendChild(newNode)
+    return newNode
+}
+function build_json(){
+  let i = 1;
+  let input_type;
+  let metadata = "";
+  let data = []
+  for(i;i<=tableID;i++){//console.log((document.querySelector('.item.active.selected').childNodes[1].id))
+      //console.log(gestures[1].toString())
+      let input = document.querySelector('.item.active.selected').childNodes[1].id
+      let voice_data = null;
+      if(input == "Voice"){
+        voice_data = document.querySelector("#command"+i).value
+      }
+      let input_data = {Voice_input: voice_data}
 
   
-
-
-  exec('python hello.py', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+    possibleActions = $("#actionChoice"+i).val()
+    let type_text = null;
+    let tts_speaker = null;
+    let tts_text = null 
+    if(possibleActions == action_type[2]){
+      tts_text = document.getElementById(possibleActions+i).value
+      let speakers = document.getElementById("dropDown" + action_type[2] + i)
+      tts_speaker =  speakers.options[speakers.selectedIndex].text
+      console.log( )
     }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+    else if(action_type.includes(possibleActions,3)){
+      type_text = document.getElementById(possibleActions+i).value
+    }
+      let query = {
+        input: input,
+        input_data: input_data,
+        action: possibleActions,
+        action_data: {
+          type_text: type_text,
+          tts_speaker: tts_speaker,
+          tts_text: tts_text
+        }
+      }
+      data.push(query);
+
+  }
+  fs.open("test.json",'w', err => { 
+     
+    // Checking for errors 
+    if (err) throw err;  
+   
+    console.log("Done opening"); // Success 
 });
-  win.webContents.openDevTools();
-
-})
-
-soundWinBtn.addEventListener('click', (event) => {
-  const modalPath = path.join('file://', __dirname, '../../my-app/src/sound.html')
-  let win = new BrowserWindow({ 
-    width: 1300,
-    height: 1200,
-    // kiosk: true,
-    // webSecurity: false,
-    // transparent: true,
-    // frame: false 
-    webPreferences: {
-      // Allows us to call nodejs globals in the frontend code
-      nodeIntegration: true,
-      enableRemoteModule: true,
-    },
-  })
-
-  win.on('close', () => { win = null })
-  win.loadURL(modalPath)
-  win.show()
-})
-
-
-
-// function loadtext(){
-//     let fileContents = fs.readFileSync("configuration.yml");
-//     let data = yaml.load(fileContents);
-//     document.getElementById('output').textContent="WOrk";//Object.values(data)[0]; 
-//     console.log(data);
-// }
-
-*/
-
+  fs.writeFile("config.json", JSON.stringify(data), err => { 
+ 
+    // Checking for errors 
+    if (err) throw err;  
+   
+    console.log("Done writing"); // Success 
+});
+}
